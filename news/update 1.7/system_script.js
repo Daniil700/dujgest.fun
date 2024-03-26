ca7f9093f0b785b07277dc5f939cb078f56978c9
Daniil700 <81581781+Daniil700@users.noreply.github.com>
2024-03-27 00:45:14 +0200

    Add files via upload

---
+++ news/update 1.7/system_script.js
@@ -0,0 +1,14 @@
+const menuIcon = document.querySelector('#menu-icon');
+const mainMenu = document.querySelector('.mainMenu');
+
+menuIcon.onclick = () => {
+menuIcon.classList.toggle('bx-x');
+mainMenu.classList.toggle('active');
+};
+
+
+
+
+
+
+
